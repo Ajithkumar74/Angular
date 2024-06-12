bf73e6104bfef38fb4abf46ab4a96d09bebbf50d
Ajithkumar74 <70085441+Ajithkumar74@users.noreply.github.com>
2024-06-12 10:56:26 +0530

    basron_invcom

--- BASRON_INVCOM/src/app/app.component.ts
+++ BASRON_INVCOM/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { CommonModule } from '@angular/common';
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule,ToastModule,CommonModule],
+  imports: [RouterOutlet,HttpClientModule,ToastModule,CommonModule,BrowserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
